2bb0f309b66eb2e36a05228ae83920a7feadd43d
cristic1988 <cristic8@gmail.com>
2024-03-04 23:43:32 +0200

    add new system out println

--- src/GitLesson04.java
+++ src/GitLesson04.java
@@ -1,6 +1,8 @@
 public class GitLesson04 {
     public static void main (String[] args){
         System.out.println("Hello git");
+        System.out.println("second changes for git");
+
 
 
     }
